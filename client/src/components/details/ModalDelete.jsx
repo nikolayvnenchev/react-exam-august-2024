68f967a14273e16cd8dd9342b7a7ec2b73762e04
nikolayvnenchev <niki93sci@abv.bg>
2024-08-02 23:46:49 +0300

    ModalDelete component implementation

--- client/src/components/details/ModalDelete.jsx
+++ client/src/components/details/ModalDelete.jsx
@@ -1,5 +1,4 @@
 import { Dialog, DialogPanel } from '@headlessui/react'
-import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
 import { useState } from 'react'
 
 export default function ModalDelete({ onCancel, onConfirm }) {
