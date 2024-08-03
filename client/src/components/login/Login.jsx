af890664c5e7307649b6b4559084ead02f387692
nikolayvnenchev <niki93sci@abv.bg>
2024-08-03 20:59:24 +0300

    Style changes in Login and Register components done

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -47,7 +47,7 @@ export default function Login() {
 
     return (
         <>
-            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div style={{ height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: '12px' }}>
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                     <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
                         Sign in to your account
