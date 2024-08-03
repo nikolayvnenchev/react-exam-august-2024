27313af8bf7fb694fe23803238a149018e329296
nikolayvnenchev <niki93sci@abv.bg>
2024-08-03 20:57:40 +0300

    Style changes in Footer component done

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer
-      className="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left">
+      className="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left border-t border-neutral-300 dark:border-neutral-600">
       <div className="p-4 text-center text-neutral-700 dark:text-neutral-200">
-        This site was created as an exam project in S
+        This site was created as an exam project in&nbsp;
         <a
-          className="text-neutral-800 dark:text-neutral-400"
+          style={{ color: 'blue' }}
+          className="text-neutral-800 font-bold dark:text-neutral-400"
           href="https://softuni.bg/"
-        >oftUni</a>
+        >SoftUni</a>
       </div>
     </footer>
   );
